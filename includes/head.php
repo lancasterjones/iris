767f887b777b23f5955658828b32e93c897a0a37
Daniel Pérez <daniel@vende.io>
2015-04-06 14:05:57 -0500

    Incluir head, logo y script

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<?php
+echo '
+	<meta charset="UTF-8">
+	<!-- Latest compiled and minified CSS -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- jQuery -->
+    <script type="text/javascript" charset="utf8" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!--jquery ui-->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="http://vende.io/wp-content/themes/primary/favicon.ico" />
+    <!--ANGULAR-->
+    <script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+
+';
+?>
--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<h1>Hola mundo</h1>
+<?php
+	//se inicia sesión
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang='es'>
+	<head>
+		<title>Bienvenido || LOB</title>
+		<?php 
+			  include 'includes/head.php';
+		?>     
+	</head>
+	<body>
+		<div id="divForm" align="center" class='container'>
+			<div class="container">
+				</br></br></br>
+				<img class="pull-left" src="includes/vende.png">
+				<div class="pull-right"><h2>IRIS</h2></div>
+				<h1>Bienvenido</h1>	</br>
+				<h4>Ingresa tu usuario y contraseña para poder acceder a la aplicación.</h4>			
+			</div>
+			</br><hr></br>
+
+			<form action="validacion.php" method="post" class="form-horizontal" role='form'>
+				<!--Formulario para usuario-->
+				<div class='form-group'>
+				    <label class="control-label col-sm-5" for="usuario">Usuario:</label>
+				    <div class="col-sm-4">			    
+				    	<input class="form-control" name="usuario" type="text" id="usuario" placeholder="Usuario">    
+				    </div>
+			  	</div>	
+			  	<!--Formulario para contraseña-->
+			  	<div class="form-group">			
+					<label class="control-label col-sm-5" for="password">Contraseña:</label>
+					<div class="col-sm-4">
+				    	<input class="form-control" name="pas" type="password" id="password" placeholder="Password">
+				    </div>				    
+				</div></br>
+				<!--Botón ingresar-->
+				<div style="max-width: 200px;">
+					<button ="ingresar" class="btn btn-success btn-block">
+					<span class="glyphicon glyphicon-lock"></span>  Ingresar
+					</button>
+				</div>
+					<hr>
+			</form>	
+			</div>
+		</div>
+		
+	</body>
+</html>

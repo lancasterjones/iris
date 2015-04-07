1b7ec4cccec286b54d19f7f56519772906aca0ec
Daniel Pérez <daniel@vende.io>
2015-04-07 13:34:05 -0500

    se corrige nombre de tabla usuarios

--- includes/data_base.php
+++ includes/data_base.php
@@ -3,8 +3,16 @@
     $db_usuario = 'admin_fotos';
     $db_password = "9Fdvi3D4LR";
     $db_basedatos = 'admin_sistemaproductos';
-    $db_tabla = 'open_to_sell';
-    $nombre_fichero = 'View';
+    $db_tabla = 'usuarios';
     //conexión a base de datos
-    $dblink = mysqli_connect($db_host, $db_usuario, $db_password, $db_basedatos) or die("No puede conectar " . mysql_error());
+    define("CAN_REGISTER", "any");
+	define("DEFAULT_ROLE", "member");
+	define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+	// Conectar
+	$conn = new mysqli($db_host, $db_user, $db_password, $db_basedatos) or die("Some error occurred during connection " . mysqli_error($conn));
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Conexión a Shampions DB falló: " . $conn->connect_error);
+	}
+
 ?>

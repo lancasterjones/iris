47e417a5d487183c481a25789bd81e43e339d550
Daniel Pérez <daniel@vende.io>
2015-04-07 13:44:06 -0500

    Se crea archivo para procesar login

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -3,7 +3,7 @@
 	$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 		if($mysqli->connect_error)
 		{
-			header("Location: ../error.php?err=Unable to connect to MySQL");
+			header("Location: error.php?err=Unable to connect to MySQL");
 			exit();
 		}		
 ?>

943da06478cec64acb06970c295e5b46015c565d
lancasterjones <lancasterjones@gmail.com>
2015-04-06 19:39:41 -0500

    Se incluye número de pedidos

--- reportebasico.php
+++ reportebasico.php
@@ -13,6 +13,7 @@
   <?php require 'includes/db_magento_connect.php';
           $semana = date("W")-2;
           $query = "SELECT 
+            count(sales_flat_order.total_paid) Pedidos
             sum(sales_flat_order.total_paid) Venta
             ,WEEK(sales_flat_order.created_at) Semana
             ,YEAR(sales_flat_order.created_at) Año
@@ -24,11 +25,24 @@
           $result = mysqli_query($connm,$query);
           $i = 0;
             while( $row = mysqli_fetch_array($result)) {
-            if ($i == 0){
               $venta = $row['Venta'];
-            }
+              $pedidos = $row['Pedidos']
           }
           mysqli_free_result($result);
+
+           $fraudes = "SELECT 
+                         COUNT(sales_flat_order.total_paid) AS Pedidos,
+                         SUM(sales_flat_order.total_paid) AS Venta,
+                         WEEK(sales_flat_order.created_at) AS Semana,
+                         YEAR(sales_flat_order.created_at) AS Año
+                        FROM shop_production.sales_flat_order sales_flat_order
+                        WHERE (    (    sales_flat_order.status IN ('riskified_declined')
+                          AND YEAR(sales_flat_order.created_at) = YEAR(CURDATE()))
+                          AND WEEK(sales_flat_order.created_at) = 13)
+                      ";
+
+
+
           mysqli_close($connm);
         ?>
 
@@ -79,7 +93,10 @@
               <h3 class="panel-title">Ventas</h3>
             </div>
             <div class="panel-body">
-              Venta: $<?php echo $venta; ?>
+              <p>Monto Venta: $<?php echo $venta; ?></p>
+              <p>Pedidos: <?php echo $pedidos; ?></p>
+              <p>Fraude: </p>
+              <p>Acumulado del Mes: </p>
             </div>
           </div>
 

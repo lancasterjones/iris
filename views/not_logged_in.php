2e422ad579c9b957f9a00e2fa90b26a6d8bf806e
Daniel Pérez <daniel@vende.io>
2015-04-07 16:16:23 -0500

    estilo bootstrap para formulario de ingreso

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -26,21 +26,26 @@
         <form class="form-horizontal" method="post" action="index.php" name="loginform">
             <!--Input ingresar usuario-->
             <div class='form-group'>
-                <label class="control-label col-sm-5" for="login_input_username">Usuario</label>
+                <label class="control-label col-sm-5" for="login_input_username">Usuario: </label>
                 <div class="col-sm-4">
+                    <input placeholder="Usuario" id="login_input_username" class="login_input" type="text" name="user_name" required />
                 </div>
             </div>
 
             <!--Input ingresar contraseña-->
-            <label for="login_input_password">Contraseña</label>
-            <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
-
+            <div class='form-group'>
+                <label class="control-label col-sm-5" for="login_input_password">Contraseña</label>
+                <div class="col-sm-4">
+                    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+                </div>
+            </div>
 
             <!--boton ingresar-->
-            <div style="max-width: 200px;">
-                <span class="glyphicon glyphicon-lock"></span>
-                <input type="submit" class="btn btn-success btn-block" name="login" value="Ingresar" />
+            <div style="max-width: 200px;">                
+                <button type="submit" class="btn btn-success btn-block" name="login" value="Ingresar"> Ingresar
+                <span class="glyphicon glyphicon-lock"></span></button>
             </div>
+            <hr>
 
         </form>
     </body>

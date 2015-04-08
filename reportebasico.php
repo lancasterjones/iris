cea6346237c54e83d95beb4c78941a1446d0d7c1
Daniel Pérez <daniel@vende.io>
2015-04-08 11:36:18 -0500

    Se llama a script para inicializar piechart

--- reportebasico.php
+++ reportebasico.php
@@ -40,6 +40,7 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <script type="text/javascript" src="includes/Chart.js"></script>
+  <script type="text/javascript" src="includes/script_reportes.js"></script>
 	</head>
 	<body>
   <?php require 'includes/db_magento_connect.php';

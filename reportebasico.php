40bae0e6b80515b0aaca4d398f5d122b4233b25e
Daniel Pérez <daniel@vende.io>
2015-04-08 10:01:17 -0500

    Se crea script externo para menu de navegación

--- reportebasico.php
+++ reportebasico.php
@@ -77,44 +77,10 @@
           mysqli_close($connm);
         ?>
 
-	<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Iris</a>
-    </div>
+	<?php
+    //menu de navegación
+   include 'includes/menu.php' ?>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Inicio <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">KPIs</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Reportes Semanales <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Última Semana</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Anteriores</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Buscar en sitio...">
-        </div>
-        <button type="submit" class="btn btn-default">Buscar</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="index.php?logout">Logout</a></li>
-        <li><a href="http://www.vende.io">Vende.io</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
 		<div class="container">
 			<div class="jumbotron">
 			 <h1>Reporte de Ventas Semana: <?php echo $semana; ?> </h1>

dc0fcbac447da130e91e0addd86781623263386c
alexventas <lancasterjones@gmail.com>
2015-04-09 13:39:26 -0500

    Solo traer fotos con posición 1
    Para procurar traer las fotos principales de cada producto

--- includes/querymasvendidos.php
+++ includes/querymasvendidos.php
@@ -9,23 +9,27 @@ $query = "SELECT CONCAT(
        sales_bestsellers_aggregated_daily.product_price AS precio,
        catalog_product_entity_media_gallery.value AS foto,
        cataloginventory_stock_item.qty
-  FROM ((((shop_production.sales_bestsellers_aggregated_daily sales_bestsellers_aggregated_daily
+  FROM (((((shop_production.sales_bestsellers_aggregated_daily sales_bestsellers_aggregated_daily
+            INNER JOIN
+            shop_production.catalog_product_entity catalog_product_entity
+               ON (sales_bestsellers_aggregated_daily.product_id =
+                      catalog_product_entity.entity_id))
            INNER JOIN
-           shop_production.catalog_product_entity catalog_product_entity
-              ON (sales_bestsellers_aggregated_daily.product_id =
-                     catalog_product_entity.entity_id))
+           shop_production.catalog_product_relation catalog_product_relation
+              ON (catalog_product_entity.entity_id =
+                     catalog_product_relation.child_id))
           INNER JOIN
-          shop_production.catalog_product_relation catalog_product_relation
-             ON (catalog_product_entity.entity_id =
-                    catalog_product_relation.child_id))
+          shop_production.catalog_product_entity catalog_product_entity_1
+             ON (catalog_product_relation.parent_id =
+                    catalog_product_entity_1.entity_id))
          INNER JOIN
-         shop_production.catalog_product_entity catalog_product_entity_1
-            ON (catalog_product_relation.parent_id =
-                   catalog_product_entity_1.entity_id))
+         shop_production.catalog_product_entity_media_gallery catalog_product_entity_media_gallery
+            ON (catalog_product_entity_1.entity_id =
+                   catalog_product_entity_media_gallery.entity_id))
         INNER JOIN
-        shop_production.catalog_product_entity_media_gallery catalog_product_entity_media_gallery
-           ON (catalog_product_entity_1.entity_id =
-                  catalog_product_entity_media_gallery.entity_id))
+        shop_production.catalog_product_entity_media_gallery_value catalog_product_entity_media_gallery_value
+           ON (catalog_product_entity_media_gallery.value_id =
+                  catalog_product_entity_media_gallery_value.value_id))
        INNER JOIN
        shop_production.cataloginventory_stock_item cataloginventory_stock_item
           ON (cataloginventory_stock_item.product_id =
@@ -33,6 +37,7 @@ $query = "SELECT CONCAT(
  WHERE     (YEARWEEK(sales_bestsellers_aggregated_daily.period) >
                YEARWEEK(CURDATE()) - 12)
        AND (sales_bestsellers_aggregated_daily.qty_ordered > 1)
+       AND (catalog_product_entity_media_gallery_value.position = 1)
 GROUP BY CONCAT(
             YEAR(sales_bestsellers_aggregated_daily.period),
             IF(MONTH(sales_bestsellers_aggregated_daily.period) < 10,

cf7ba65d4b155b8838aef5eb762f0d7f7533c862
Daniel Pérez <daniel@vende.io>
2015-04-09 14:36:03 -0500

    Se elimina conexion manual a bd magento
    se agrega include unicamente

--- includes/actualizar_bd.php
+++ includes/actualizar_bd.php
@@ -5,22 +5,6 @@
 		include 'includes/querymasvendidos.php';
 
 
-		$servername = "54.215.253.12";
-$username = "databaseread";
-$password = "N46g3ta6skXqbete";
-$db = "shop_production";
-
-// Conectar
-$connm = new mysqli($servername, $username, $password, $db) or die("Error al conectar a Magento " . mysqli_error($connm));
-// Check connection
-if ($connm->connect_error) {
-    die("Conexión a DB Sistema Productos falló: " . $connm->connect_error);
-} else
-{
-	echo "conexión correcta!";
-}
-
-
 		$query = "
 		SELECT CONCAT(
           YEAR(sales_bestsellers_aggregated_daily.period),

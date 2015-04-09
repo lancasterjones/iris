e221d6133bdd9088094960e4a5700ca8aad76aba
Daniel Pérez <daniel@vende.io>
2015-04-09 15:39:41 -0500

    Se imprime sku

--- reportes_anteriores.php
+++ reportes_anteriores.php
@@ -188,7 +188,7 @@
                       </td>
                   </tr>
                   <tr>
-                      <td>SRL-4086</td>
+                      <td>'. $consulta['sku'] .'</td>
                       <td><img class="pic" src="http://d1x736u1i353au.cloudfront.net/media/catalog/product/cache/4/thumbnail/9df78eab33525d08d6e5fb8d27136e95/d/v/dvcr0210.jpg"></td>
                       <td>
                           <div align="center" class="media-middle">

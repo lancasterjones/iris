af43c50a5f73ab3ac82325914b8a7f73899e40ed
Daniel Pérez <daniel@vende.io>
2015-04-09 10:31:53 -0500

    Se pasa valor a variable semana a través de url

--- reportes_anteriores.php
+++ reportes_anteriores.php
@@ -179,9 +179,9 @@
                     $fin = $week - 10;
                        for($sem = $week; $sem > $fin; $sem--)
                        {
-                          echo "<tr><td>Semana ";
+                          echo "<form action='download_view.php' method='post'><tr><td>Semana ";
                           echo $sem;
-                          echo "<i class='glyphicon glyphicon-cloud-download pull-right'></i></td></tr>";  
+                          echo "<a href='download_view.php?semana=".$sem."'><i class='glyphicon glyphicon-cloud-download pull-right'></i></a></td></tr></form>";  
                        }
                 ?>                
             </tr>

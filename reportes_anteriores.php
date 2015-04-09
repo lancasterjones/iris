f679941a2f83be84f09f40dfdc4f0235e522bf42
Daniel Pérez <daniel@vende.io>
2015-04-09 15:34:46 -0500

    Se actualiza nombre de array

--- reportes_anteriores.php
+++ reportes_anteriores.php
@@ -179,7 +179,7 @@
                     </form>
                   </tr>
                   <tr>
-                      <td>'. $contador[0][0] .'</td>
+                      <td>'. $contenedor[0][0] .'</td>
                       <td><img class="pic" src="http://d1x736u1i353au.cloudfront.net/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/d/v/dvcr0210_2.jpg"></td>
                       <td>
                         <div align="center" class="media-middle">

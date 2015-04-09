86ae32f28cc60bc20e1897ba3f75ec777f002313
Daniel Pérez <daniel@vende.io>
2015-04-09 14:39:34 -0500

    Se almacena e imprime toda la consulta

--- includes/actualizar_bd.php
+++ includes/actualizar_bd.php
@@ -4,6 +4,17 @@
 	//traer información
 		include 'includes/querymasvendidos.php';
 
+$servername = "54.215.253.12";
+$username = "databaseread";
+$password = "N46g3ta6skXqbete";
+$db = "shop_production";
+
+// Conectar
+$connm = new mysqli($servername, $username, $password, $db) or die("Error al conectar a Magento " . mysqli_error($connm));
+// Check connection
+if ($connm->connect_error) {
+    die("Conexión a DB Sistema Productos falló: " . $connm->connect_error);
+} 
 
 		$query = "
 		SELECT CONCAT(
@@ -62,6 +73,9 @@ ORDER BY 1 DESC, sales_bestsellers_aggregated_daily.qty_ordered DESC";
 		while ($consulta = mysqli_fetch_array($result)) {
 			$contenedor[$contador][0] = $consulta['foto'];
       $contenedor[$contador][1] = $consulta['mes'];
+      $contenedor[$contador][2] = $consulta['precio'];
+      $contenedor[$contador][3] = $consulta['sku'];
+      $contenedor[$contador][4] = $consulta['qty'];
       $contador++;
 		}
 		

a94ef8863d566bfc2bc714a41dcdc6225887dd1e
lancasterjones <lancasterjones@gmail.com>
2015-04-23 17:35:12 -0500

    Ejemplo 2

--- googleapitest.php
+++ googleapitest.php
@@ -1,4 +1,25 @@
 <?php
+
+ 
+/*
+
+  $client = new Google_Client();
+$client->setClientId(421555446319-0h80flbkju1vqagjkbsc9jl7ahjoubug.apps.googleusercontent.com);
+$client->setClientSecret(JqHvAtOgzkbM_CqrxGL1MhU0);
+$client->setRedirectUri($redirect_uri);
+
+  $service = new Google_Service_Books($client);
+  $optParams = array('filter' => 'free-ebooks');
+  $results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+
+  foreach ($results as $item) {
+    echo $item['volumeInfo']['title'], "<br /> \n";
+  }
+
+echo "Hello World";
+
+*/
+
 /*
  * Copyright 2013 Google Inc.
  *
@@ -24,7 +45,7 @@ echo pageHeader("Simple API Access");
   should use our quota, which is higher than the
   anonymous quota (which is limited per IP).
  ************************************************/
-require_once realpath(dirname(__FILE__) . '/../src/Google/autoload.php');
+require_once '/libraries/GoogleClientAPI/src/Google/autoload.php'; // or wherever autoload.php is located
 /************************************************
   We create the client and set the simple API
   access key. If you comment out the call to
@@ -77,3 +98,45 @@ foreach ($results as $item) {
   echo $item['volumeInfo']['title'], "<br /> \n";
 }
 echo pageFooter(__FILE__);
+/*
+require_once ('/libraries/GoogleClientAPI/src/Google/Client.php');
+require_once ('/libraries/GoogleClientAPI/src/Google/Service/Analytics.php');
+
+
+$scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+
+$client = new Google_Client();
+$client->setAccessType('online'); // default: offline
+$client->setApplicationName('Extractor de LOB a Vende');
+$client->setClientId('421555446319-0h80flbkju1vqagjkbsc9jl7ahjoubug.apps.googleusercontent.com');
+$client->setClientSecret('JqHvAtOgzkbM_CqrxGL1MhU0');
+$client->setRedirectUri($scriptUri);
+$client->setDeveloperKey('AIzaSyDfOXaHjoTSQbIOHX-p6HROTOZmIS4QIEA'); // API key
+
+// $service implements the client interface, has to be set before auth call
+$service = new Google_AnalyticsService($client);
+
+if (isset($_GET['logout'])) { // logout: destroy token
+    unset($_SESSION['token']);
+	die('Logged out.');
+}
+
+if (isset($_GET['code'])) { // we received the positive auth callback, get the token and store it in session
+    $client->authenticate();
+    $_SESSION['token'] = $client->getAccessToken();
+}
+
+if (isset($_SESSION['token'])) { // extract token from session and configure client
+    $token = $_SESSION['token'];
+    $client->setAccessToken($token);
+}
+
+if (!$client->getAccessToken()) { // auth call to google
+    $authUrl = $client->createAuthUrl();
+    header("Location: ".$authUrl);
+    die;
+} 
+
+echo 'Hello, world.';
+
+?>

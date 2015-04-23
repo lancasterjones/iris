b7a428a8f570cf9dba52080f8fbec3dfa72bb9de
lancasterjones <lancasterjones@gmail.com>
2015-04-23 17:13:03 -0500

    Debug 2

--- googleapitest.php
+++ googleapitest.php
@@ -1,5 +1,5 @@
 <?php
-//session_start();
+session_start();
 /* require_once ('/libraries/GoogleClientApi/Google_Client.php');
 require_once ('/libraries/GoogleClientApi/contrib/Google_AnalyticsService.php');
 

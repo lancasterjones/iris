e7064dda29e9f3ed4885b404d84c08f67e411b56
lancasterjones <lancasterjones@gmail.com>
2015-04-23 17:08:42 -0500

    Cambio de nombre de aplicación

--- googleapitest.php
+++ googleapitest.php
@@ -7,7 +7,7 @@ $scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
 
 $client = new Google_Client();
 $client->setAccessType('online'); // default: offline
-$client->setApplicationName('LOB a Vende');
+$client->setApplicationName('Extractor de LOB a Vende');
 $client->setClientId('421555446319-0h80flbkju1vqagjkbsc9jl7ahjoubug.apps.googleusercontent.com');
 $client->setClientSecret('JqHvAtOgzkbM_CqrxGL1MhU0');
 $client->setRedirectUri($scriptUri);

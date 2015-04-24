5e85c3bfe86f19a67821c2483013380736df0c98
Daniel Pérez <daniel@vende.io>
2015-04-24 14:44:25 -0500

    se actualiza script de conexion a db

--- includes/data_base.php
+++ includes/data_base.php
@@ -6,7 +6,7 @@
     $db_tabla = 'usuarios';
     //conexión a base de datos
 	// Conectar
-	$conn = new mysqli($db_host, $db_user, $db_password, $db_basedatos) or die("Some error occurred during connection " . mysqli_error($conn));
+	$conn = new mysqli($db_host, $db_usuario, $db_password, $db_basedatos) or die("Some error occurred during connection " . mysqli_error($conn));
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Conexión a Shampions DB falló: " . $conn->connect_error);

a29dc50691a6efb103bc554c66acfdd203d62216
Daniel Pérez <daniel@vende.io>
2015-04-27 11:53:55 -0500

    se crea funcion para crear menu meses anteriores

--- includes/side_menu.php
+++ includes/side_menu.php
@@ -1,3 +1,9 @@
+<?php 
+	//año actual
+    $current_year = date("Y");
+    $mes_actual = date('n');
+?>
+
 <nav class="navbar navbar-inverse sidebar" role="navigation">
     <div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
@@ -17,10 +23,28 @@
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Otros Meses <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-calendar"></span></a>
 					<ul class="dropdown-menu forAnimate" role="menu">
-						<li><a style="color:white;" href="?mes=1">Enero</a></li>
-						<li><a style="color:white;" href="?mes=2">Febrero</a></li>
-						<li><a style="color:white;" href="?mes=3">Marzo</a></li>
-						<li><a style="color:white;" href="?mes=4">Abril</a></li>
+						<?php 
+							for($x = $mes_actual; $x <= 1; $x--){
+								//este switch cambia el numero por una palabra para identificar el nombre del mes
+							    switch($mes_actual){
+							        case 1: $mes = "Enero "; break;
+							        case 2: $mes = "Febrero "; break;
+							        case 3: $mes = "Marzo "; break;
+							        case 4: $mes = "Abril "; break;
+							        case 5: $mes = "Mayo "; break;
+							        case 6: $mes = "Junio "; break;
+							        case 7: $mes = "Julio "; break;
+							        case 8: $mes = "Agosto "; break;
+							        case 9: $mes = "Septiembre "; break;
+							        case 10: $mes = "Octubre "; break;
+							        case 11: $mes = "Noviembre "; break;
+							        case 12: $mes = "Diciembre "; break;
+							    }
+						?>
+						<li><a style="color:white;" href="?mes=<?php echo $x;?>"><?php echo $mes . $current_year?> </a>
+						<?php
+						  }
+						?>
 					</ul>
 				</li>
 				<li ><a href="#">Categorías<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicons-pie-chart"><i class="fa fa-pie-chart"></i></span></a></li>

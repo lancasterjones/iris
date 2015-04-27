a60324cf2c29dccc1aad6d092ba21d4658b6df54
Daniel Pérez <daniel@vende.io>
2015-04-27 13:10:31 -0500

    se crea script para actulizar

--- includes/side_menu.php
+++ includes/side_menu.php
@@ -48,9 +48,16 @@
 				<li ><a href="#">Categorías<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicons-pie-chart"><i class="fa fa-pie-chart"></i></span></a></li>
 				<li ><a href="#">Los Más Vendidos<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-star-empty"></span></a></li>
 				<li ><a href="#">Los Más Vistos<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-star"></span></a></li>
-				<li><a href="#">Sincronizar<i style="font-size: 16px;" class="glyphicon glyphicon-refresh hidden-xs pull-right"></i></a></li>
-				<li><a href="index.php?logout">Salir</a></li>
+				<li><a href="#" onclick="actulizar();">Sincronizar<i style="font-size: 16px;" class="glyphicon glyphicon-copy hidden-xs pull-right"></i></a></li>
+				<li><a href="index.php?logout">Salir <i class="glyphicon glyphicon-log-out hidden-xs pull-right"></i></a></li>
 			</ul>
 		</div>
 	</div>
 </nav>
+
+<script>
+	function actulizar(){
+		$.get("actualizar_bd.php");
+		return false;
+	}
+</script>

663f7354c193bb49b2d237ba9c2661d932cd5e36
alexventas <lancasterjones@gmail.com>
2015-04-28 14:05:52 -0500

    Query integrando pedidos y fraudes

--- includes/queries_reportes.php
+++ includes/queries_reportes.php
@@ -2,24 +2,39 @@
 
 	//ventas y pedidos
 	$query_reportes = "
-			SELECT
-    count(sales_flat_order.total_paid) Pedidos
-    ,sum(sales_flat_order.total_paid) Venta
-    ,WEEK(sales_flat_order.created_at,7) Semana
-    ,LEFT(YEARWEEK(sales_flat_order.created_at,7),4) Year
+			SELECT COUNT(sales_flat_order.total_paid) AS Pedidos,
+       SUM(sales_flat_order.total_paid) AS Venta,
+       WEEK(sales_flat_order.created_at, 7) AS Semana,
+       LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) AS `Year`,
+       Subquery.fraudes
   FROM shop_production.sales_flat_order sales_flat_order
-  WHERE sales_flat_order.status IN ('complete', 'processing')
-     GROUP BY YEARWEEK(sales_flat_order.created_at,7), WEEK(sales_flat_order.created_at,7)";
+       LEFT OUTER JOIN
+       (SELECT COUNT(DISTINCT sales_flat_order.increment_id) AS fraudes,
+               WEEK(sales_flat_order.created_at, 7) AS semana,
+               LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4)
+                  AS ```year```
+          FROM shop_production.sales_flat_order_status_history sales_flat_order_status_history
+               INNER JOIN shop_production.sales_flat_order sales_flat_order
+                  ON (sales_flat_order_status_history.parent_id =
+                         sales_flat_order.entity_id)
+         WHERE sales_flat_order_status_history.comment LIKE '%declined%'
+        GROUP BY WEEK(sales_flat_order.created_at, 7),
+                 LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4)) Subquery
+          ON     (LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) =
+                     Subquery.```year```)
+             AND (WEEK(sales_flat_order.created_at, 7) = Subquery.semana)
+ WHERE sales_flat_order.status IN ('complete', 'processing')
+GROUP BY WEEK(sales_flat_order.created_at, 7)";
 
 
      //query para fraudes
 
-    $fraudes = "
+   /* $fraudes = "
 			SELECT 
              COUNT(sales_flat_order.total_paid) AS Pedidos,
              WEEK(sales_flat_order.created_at, 1) AS Semana,
              YEAR(sales_flat_order.created_at) AS Year
             FROM shop_production.sales_flat_order sales_flat_order
             WHERE (    sales_flat_order.status IN ('riskified_declined')         )
-            GROUP BY Semana, Year";
+            GROUP BY Semana, Year"; */
 ?>

be03ae32c0b948b08c7e2acd38059db4276c6844
Daniel Pérez <daniel@vende.io>
2015-04-29 12:30:24 -0500

    prueba de condicion para inventario ok

--- reporte_mensual.php
+++ reporte_mensual.php
@@ -368,10 +368,14 @@
                             echo $cont_vistos[$y][0]; ?>" alt="Image" style="max-width:100%;">
                           </a>
                           <?php  if($cont_vistos[$y][2] == 0){ //se muestra cuando el inventario es cero ?>
-                          <div style="position: relative; text-align:center; width: 100%; background-color: #D10056; color: white; high: 15px; margin: 2px auto; border-radius: 0 0 5px 2px;">
-                              <p>Sin Inventario</p>
+                              <div style="position: relative; text-align:center; width: 100%; background-color: #D10056; color: white; high: 15px; margin: 2px auto; border-radius: 0 0 5px 2px;">
+                                  <p>Sin Inventario</p>
+                              </div>
+                          <?php } //cierre sin inv
+                                else{ ?>
+                                  <div style="position: relative; text-align:center; width: 100%; background-color: #18bc9c; color: white; high: 15px; margin: 2px auto; border-radius: 0 0 5px 2px;">
+                                  <p>Inventario <i style="margin-right: 3px;" class="glyphicon glyphicon-ok "></i></p>
                           </div>
-                          <?php } //cierre sin inv ?>
                           <h4 style="position: relative; margin: 0px 10%;"><?php 
                           echo $cont_vistos[$y][1]; ?></h4>
                         </div>  

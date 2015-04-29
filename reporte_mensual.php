aed05437dd0a9a2933be82326ab0533c2e4d7028
Daniel Pérez <daniel@vende.io>
2015-04-29 15:00:25 -0500

    mod ub los + vend

--- reporte_mensual.php
+++ reporte_mensual.php
@@ -214,10 +214,10 @@
         <div id="piechart" style="min-width: 310px; height: 400px; max-width: 600px; margin: 20% auto auto 35%"></div>
       </br>
         <!--Slide los más vendidos-->
-        <div style="color: black; float: left; margin: 40% 0 0 30%;">
+        <div style="color: black; float: left; margin: 30% 0 0 30%;">
           <h1>Lo + Vendido</h1>
         </div>
-        <div class="container" id="losMasVendidos" style="width: 70% !important; margin: 65% auto 0 20%;">
+        <div class="container" id="losMasVendidos" style="width: 70% !important; margin: 40% auto 0 20%;">
         <div class="row">
         <div class="col-md-12">
                     <div id="Carousel" class="carousel slide">

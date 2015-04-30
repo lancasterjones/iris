9e5ebc977bbcb52e1930f59672f5ece2615bf63e
Daniel Pérez <daniel@vende.io>
2015-04-30 14:36:20 -0500

    Revert "valor fijo de resta"
    This reverts commit 5969ff44c48f679d0459f9f62ef63d904f3af378.

--- reporte_mensual.php
+++ reporte_mensual.php
@@ -191,7 +191,7 @@
             $limit_vistos = 10;
           }
           //====================Script para grafica de dona===========================
-          $weekDona = date('W')-5;
+          $weekDona = date('W')-$columnasReporte;
           $query_dona = "SELECT * FROM magento_venta WHERE week = $weekDona AND year = $current_year";
           $res_dona = mysqli_query($conn, $query_dona);
           $x = 0;

b563f75508e8563ff1b28088eb02d70c6406a5ca
Daniel Pérez <daniel@vende.io>
2015-05-05 11:00:52 -0500

    formato para todos los labels

--- views/register.php
+++ views/register.php
@@ -35,15 +35,15 @@ if (isset($registration)) {
 
             <div class='form-group'>
                 <!-- the email input field uses a HTML5 email type check -->
-                <label class="control-label col-sm-5" for="login_input_email">Compañía</label>
+                <label class="col-lg-2 control-label" for="login_input_email">Compañía: </label>
                 <input id="login_input_email" class="form-control" type="text" name="user_email" required />
             </div>
             <div class='form-group'>
-                <label class="control-label col-sm-5" for="login_input_password_new">Contraseña: </label>
+                <label class="col-lg-2 control-label" for="login_input_password_new">Contraseña: </label>
                 <input id="login_input_password_new" class="form-control" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
             </div>
             <div class='form-group'>
-                <label class="control-label col-sm-5" for="login_input_password_repeat">Repetir Contraseña: </label>
+                <label class="col-lg-2 control-label" for="login_input_password_repeat">Repetir Contraseña: </label>
                 <input id="login_input_password_repeat" class="form-control" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
                 <input type="submit"  class="btn btn-success" name="register" value="Registrar" />
             </div>

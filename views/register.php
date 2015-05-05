8e86f2729b50b6cab6196f332fd9cd3934682e2c
Daniel Pérez <daniel@vende.io>
2015-05-05 10:55:41 -0500

    se crea titulo de form

--- views/register.php
+++ views/register.php
@@ -23,27 +23,29 @@ if (isset($registration)) {
 
     <!-- register form -->
     <form method="post" action="register.php" class="form-horizontal" role="form" name="registerform">
-        <div class='form-group'>
-            <!-- the user name input field uses a HTML5 pattern check -->
-            <label class="control-label col-sm-5" for="login_input_username">Usuario: </label>
-            <input id="login_input_username" class="form-control" type="text" name="user_name" required />
-        </div>
+        <fieldset>
+            <legend>Registro de nuevos usuarios</legend>
+            <div class='form-group'>
+                <!-- the user name input field uses a HTML5 pattern check -->
+                <label class="control-label col-sm-5" for="login_input_username">Usuario: </label>
+                <input id="login_input_username" class="form-control" type="text" name="user_name" required />
+            </div>
 
-        <div class='form-group'>
-            <!-- the email input field uses a HTML5 email type check -->
-            <label class="control-label col-sm-5" for="login_input_email">Compañía</label>
-            <input id="login_input_email" class="form-control" type="text" name="user_email" required />
-        </div>
-        <div class='form-group'>
-            <label class="control-label col-sm-5" for="login_input_password_new">Contraseña: </label>
-            <input id="login_input_password_new" class="form-control" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-        </div>
-        <div class='form-group'>
-            <label class="control-label col-sm-5" for="login_input_password_repeat">Repetir Contraseña: </label>
-            <input id="login_input_password_repeat" class="form-control" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-            <input type="submit"  class="btn btn-success" name="register" value="Registrar" />
-        </div>
-
+            <div class='form-group'>
+                <!-- the email input field uses a HTML5 email type check -->
+                <label class="control-label col-sm-5" for="login_input_email">Compañía</label>
+                <input id="login_input_email" class="form-control" type="text" name="user_email" required />
+            </div>
+            <div class='form-group'>
+                <label class="control-label col-sm-5" for="login_input_password_new">Contraseña: </label>
+                <input id="login_input_password_new" class="form-control" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+            </div>
+            <div class='form-group'>
+                <label class="control-label col-sm-5" for="login_input_password_repeat">Repetir Contraseña: </label>
+                <input id="login_input_password_repeat" class="form-control" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+                <input type="submit"  class="btn btn-success" name="register" value="Registrar" />
+            </div>
+        </fieldset>
     </form>
 
     <!-- Link para volver -->     

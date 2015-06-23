676632614750e0b55faf909996b2bb55cb90a335
danielvende <daniel@vende.io>
2015-06-23 17:07:27 -0500

    se crea script js para reporte principal

--- clientes/tecnolite/tablero_principal.php
+++ clientes/tecnolite/tablero_principal.php
@@ -1,3 +1,69 @@
-<div class="container">
-	<div style="height: 100px; width: 100px; background-color: #000;"></div>
+<div class="container-fluid">
+	<div id="reporte_principal"></div>
 </div>
+
+<script>
+	$(function () {
+        $('#reporte_principal').highcharts({
+            chart: { zoomType: 'xy' },
+            title: { text: ''       },
+            subtitle: { text: 'https://tienda.tecnolite.com.mx' },
+            xAxis: [{
+                categories: ['S1'],
+                crosshair: true
+            }],
+            yAxis: [{ 
+                min: 1,
+                labels: {
+                    format: '${value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'Ventas',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                }
+            }, { // Secondary yAxis
+                title: {
+                    text: 'Pedidos',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
+            },
+            series: [{
+                name: 'Pedidos',
+                type: 'column',
+                yAxis: 1,
+                data: [2],
+            }, {
+                name: 'Fraudes',
+                type: 'column',
+                yAxis: 1,
+                data: [5],
+                color: '#c82536'
+            }, {
+                name: 'Venta',
+                type: 'spline',
+                data: [6],
+                tooltip: {
+                    valuePrefix: '$ '
+                }
+
+            }]
+        });
+    });
+</script>

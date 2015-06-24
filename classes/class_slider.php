a42ff11196d7712626c826d4f100ed8470241cb2
danielvende <daniel@vende.io>
2015-06-24 13:06:20 -0500

    se elimina constante

--- classes/class_slider.php
+++ classes/class_slider.php
@@ -1,15 +1,15 @@
 <?php
 	class slider
 	{
-		define("maximo_en_fila", 4);
-		public $titulo;
+		private $maximo_en_fila = 4;
+		//public $titulo;
 
-		function crearSlider($titulo)
+		function crearSlider()
 		{
 ?>
 			<div id="slider" class="carousel slide" style="border-top-style: solid; border-color: #E7E7E6; border-width: 3px;">
 				<legend>
-					<?php echo $this->$titulo; ?>
+					<?php //echo $this->$titulo; ?>
 				</legend>
 				<ol class="carousel-indicators">
 					<li data-target="#slider" data-slide-to="0" class="active">
@@ -19,7 +19,7 @@
 					<div class="item active">
 						<div class="row">
 
-							<?php for($x = 1; $x <= constant("maximo_en_fila"); $x++) { ?> 
+							<?php for($x = 1; $x <= $maximo_en_fila; $x++) { ?> 
 								<div class="col-md-3">
 									<a href="#" class="thumbnail" style="margin: 0px;">
 										<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-az.jpg">
@@ -33,7 +33,7 @@
 					<div class="item">
 						<div class="row">
 
-							<?php for($x = 1; $x <= constant("maximo_en_fila"); $x++) { ?> 
+							<?php for($x = 1; $x <= $maximo_en_fila; $x++) { ?> 
 								<div class="col-md-3">
 									<a href="#" class="thumbnail" style="margin: 0px;">
 										<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-bc.jpg">
@@ -52,5 +52,5 @@
 	}//cierre de clase
 
 	$main = new slider();
-	$main->crearSlider("Lo + vendido");
+	$main->crearSlider();
 ?>

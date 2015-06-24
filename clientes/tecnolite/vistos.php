7286ce8e642c95f2f0e92cd4da6dfdef2414f8ac
danielvende <daniel@vende.io>
2015-06-24 12:03:08 -0500

    cierre de parentesis

--- clientes/tecnolite/vistos.php
+++ clientes/tecnolite/vistos.php
@@ -12,7 +12,7 @@
 		<div class="item active">
 			<div class="row">
 
-				<?php for($x = 1; $x <= constant("mostrar"; $x++)) { ?> 
+				<?php for($x = 1; $x <= constant("mostrar"); $x++) { ?> 
 					<div class="col-md-3">
 						<a href="#" class="thumbnail" style="margin: 0px;">
 							<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-az.jpg">

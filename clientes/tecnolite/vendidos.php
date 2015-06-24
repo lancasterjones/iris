e4997974c4e4bc5ee4c60602e9a40c03d2929493
danielvende <daniel@vende.io>
2015-06-24 13:48:24 -0500

    se crea condición para validar carga de clase

--- clientes/tecnolite/vendidos.php
+++ clientes/tecnolite/vendidos.php
@@ -1,5 +1,9 @@
 <?php
 	include 'http://iris.vende.io/classes/class_slider.php';
+	if((include 'http://iris.vende.io/classes/class_slider.php') == 'OK')
+	{
+		echo "Archivo cargado";
+	}
 	$main = new slider();
 	$main->crearSlider();
 ?>

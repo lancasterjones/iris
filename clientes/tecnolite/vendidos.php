f3c221546cc86488a24c7ce38aa472b917d6344a
danielvende <daniel@vende.io>
2015-06-24 13:59:03 -0500

    se cambia ruta de archivo

--- clientes/tecnolite/vendidos.php
+++ clientes/tecnolite/vendidos.php
@@ -1,6 +1,6 @@
 <?php
-	include 'http://iris.vende.io/classes/class_slider.php';
-	if((include 'http://iris.vende.io/classes/class_slider.php') == 'OK')
+	include '../../../classes/class_slider.php';
+	if((include '../../../classes/class_slider.php') == 'OK')
 	{
 		echo "Archivo cargado";
 	}else echo "No se cargó el archivo";

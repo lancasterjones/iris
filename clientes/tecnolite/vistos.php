40409ea8069efae4fc3bea5f1836bbe48e6706a1
danielvende <daniel@vende.io>
2015-06-24 12:23:26 -0500

    se añade botones para controlar slider

--- clientes/tecnolite/vistos.php
+++ clientes/tecnolite/vistos.php
@@ -1,18 +1,18 @@
 <?php
-	define("mostrar", 4);
+	define("maximo_en_fila", 4);
 ?>
 
-<div id="vistos" class="carousel slide" style="border-top-style: solid; border-color: #E7E7E6; border-width: 3px;">
+<div id="slider" class="carousel slide" style="border-top-style: solid; border-color: #E7E7E6; border-width: 3px;">
 	<legend>Lo + visto</legend>
 	<ol class="carousel-indicators">
-		<li data-target="#vistos" data-slide-to="0" class="active">
-		<li data-target="#vistos" data-slide-to="1">
+		<li data-target="#slider" data-slide-to="0" class="active">
+		<li data-target="#slider" data-slide-to="1">
 	</ol>
 	<div class="carousel-inner">
 		<div class="item active">
 			<div class="row">
 
-				<?php for($x = 1; $x <= constant("mostrar"); $x++) { ?> 
+				<?php for($x = 1; $x <= constant("maximo_en_fila"); $x++) { ?> 
 					<div class="col-md-3">
 						<a href="#" class="thumbnail" style="margin: 0px;">
 							<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-az.jpg">
@@ -26,7 +26,7 @@
 		<div class="item">
 			<div class="row">
 
-				<?php for($x = 1; $x <= constant("mostrar"); $x++) { ?> 
+				<?php for($x = 1; $x <= constant("maximo_en_fila"); $x++) { ?> 
 					<div class="col-md-3">
 						<a href="#" class="thumbnail" style="margin: 0px;">
 							<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-bc.jpg">
@@ -37,4 +37,6 @@
 			</div>
 		</div><!--Item-->
 	</div>
+	<a data-slide="prev" href="#slider" class="left carousel-control">‹</a>
+    <a data-slide="next" href="#slider" class="right carousel-control">›</a>
 </div>

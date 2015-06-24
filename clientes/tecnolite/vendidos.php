81387e63ad54d75daf0907ff2458960496d9b053
danielvende <daniel@vende.io>
2015-06-24 13:24:00 -0500

    se instancia clase slider en documento "vendidos"

--- clientes/tecnolite/vendidos.php
+++ clientes/tecnolite/vendidos.php
@@ -1,24 +1,5 @@
-<div id="vendidos" class="carousel slide" style="border-top-style: solid; border-color: #E7E7E6; border-width: 3px;">
-	<legend>Lo + vendido</legend>
-	<ol class="carousel-indicators">
-		<li data-target="#vendidos" data-slide-to="0" class="active">
-		<li data-target="#vendidos" data-slide-to="1">
-	</ol>
-	<div class="carousel-inner">
-		<div class="item active">
-			<div class="row">
-				<div class="col-md-3">
-					<a href="#" class="thumbnail" style="margin: 0px;">
-						<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-bc.jpg">
-					</a>
-				</div>
-
-				<div class="col-md-3">
-					<a href="#" class="thumbnail" style="margin: 0px;">
-						<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-az.jpg">
-					</a>
-				</div>				
-			</div>
-		</div>
-	</div>
-</div>
+<?php
+	include '../classes/class_slider.php';
+	$main = new slider();
+	$main->crearSlider("Lo + vendido");
+?>

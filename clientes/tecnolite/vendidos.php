e33702af757a1ab3ffb39e5c0e74c149d223c4e5
danielvende <daniel@vende.io>
2015-06-24 14:48:51 -0500

    cambio de ruta archivo clase

--- clientes/tecnolite/vendidos.php
+++ clientes/tecnolite/vendidos.php
@@ -1,6 +1,6 @@
 <?php
-	include '../../../classes/class_slider.php';
-	if((include '../../../classes/class_slider.php') == 'OK')
+	include '../../classes/class_slider.php';
+	if((include '../../classes/class_slider.php') == 'OK')
 	{
 		echo "Archivo cargado";
 	}else echo "No se cargó el archivo";

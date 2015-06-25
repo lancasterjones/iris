3cc0d8628d97a77bd589d48af9f8776505599de9
danielvende <daniel@vende.io>
2015-06-25 18:07:45 -0500

    se implemente validación de existecia todos elemnt

--- clientes/tecnolite/vistos.php
+++ clientes/tecnolite/vistos.php
@@ -55,13 +55,22 @@
 						<a href="#" class="thumbnail" style="margin: 0px;">
 							<img src="http://d1x736u1i353au.cloudfront.net/media/catalog/product<?php echo $foto[$x]; ?>">
 						</a>
-						<span class="col-md-offset-4 label label-success">
-							<i class="glyphicon glyphicon-ok"></i>							
-						</span>
-						<span class="col-md-offset-4 label label-danger" style="display: none"> 
-							<i class="glyphicon glyphicon-remove"></i>
-						</span>
-						<?php echo $modelo[$x]; ?>
+
+						<?php 
+							if($existencia[$x] == 1) 
+							{ 
+					     ?>
+								<span class="col-md-offset-4 label label-success">
+									<i class="glyphicon glyphicon-ok"></i>							
+								</span>
+						<?php } 
+						      elseif($existencia[$x] == 0) 
+						      { 
+						?>
+								<span class="col-md-offset-4 label label-danger"> 
+									<i class="glyphicon glyphicon-remove"></i>
+								</span>
+						<?php } echo $modelo[$x]; ?>
 					</div>		
 				<?php } ?>
 
@@ -92,7 +101,6 @@
 									<i class="glyphicon glyphicon-remove"></i>
 								</span>
 						<?php } echo $modelo[$x]; ?>
-
 					</div>		
 				<?php } ?>
 
@@ -123,7 +131,6 @@
 									<i class="glyphicon glyphicon-remove"></i>
 								</span>
 						<?php } echo $modelo[$x]; ?>
-
 					</div>		
 				<?php } //for?>
 

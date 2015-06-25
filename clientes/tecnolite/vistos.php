b494af03b955a44a94e66d36fd04eba74a8e1440
danielvende <daniel@vende.io>
2015-06-25 14:18:43 -0500

    se añade icono remove a vistos

--- clientes/tecnolite/vistos.php
+++ clientes/tecnolite/vistos.php
@@ -15,6 +15,7 @@
 							<img src="https://tienda.tecnolite.com.mx/media/catalog/product/cache/1/small_image/500x593/9df78eab33525d08d6e5fb8d27136e95/b/o/bombillas_a19led-3.5w-fil-az.jpg">
 						</a>
 						<span class="label label-warning"><i class="glyphicon glyphicon-ok"></i></span>
+						<span class="label label-danger"> <i class="glyphicon glyphicon-remove"></i></span>
 					</div>		
 				<?php } ?>
 

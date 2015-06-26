74c0beb2a6c1a7647f0745c947a7688eb63de973
danielvende <daniel@vende.io>
2015-06-26 16:15:31 -0500

    se define ruta de archivos

--- clientes/vende/menu.php
+++ clientes/vende/menu.php
@@ -1,6 +1,9 @@
 <?php
-	require_once("config/db.php");
-    require_once("classes/Login.php");
+	ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+	require_once("../../config/db.php");
+    require_once("../../classes/Login.php");
     $login = new Login();
 
 		if ($login->isUserLoggedIn() == false) 

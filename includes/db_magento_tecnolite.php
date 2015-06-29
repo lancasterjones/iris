631c4183d3f69fe17a65a9315883623dd601cc52
danielvende <daniel@vende.io>
2015-06-29 13:46:49 -0500

    conexión usuario prueba

--- includes/db_magento_tecnolite.php
+++ includes/db_magento_tecnolite.php
@@ -1,11 +1,11 @@
 <?php
 
 	$server_tecnolite  = "192.241.212.235";
-	$usuario_tecnolite = "root";
+	$usuario_tecnolite = "user_tec";
 	$pass_tecnolite    = "u8eJ[R?5dynf9@j";
 	$db_name_tecnolite = "magento";
 
-	$con_tecnolite = new mysqli($server_tecnolite, $usuario_tecnolite, $pass_tecnolite)
+	$con_tecnolite = new mysqli($server_tecnolite, $usuario_tecnolite, $pass_tecnolite, $db_name_tecnolite)
 						or die("Error " . mysqli_error($con_tecnolite));
 
 	if ($con_tecnolite->connect_error) {

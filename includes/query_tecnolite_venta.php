3d9a4473cab8582a49bc932ca9a2fde0078567df
danielvende <daniel@vende.io>
2015-06-29 14:27:39 -0500

    se actualiza query para consultar fraudes

--- includes/query_tecnolite_venta.php
+++ includes/query_tecnolite_venta.php
@@ -1,14 +1,25 @@
 <?php
 
-$query_tecnolite_venta = "SELECT sales_flat_order.total_paid,
-			       sales_flat_order.created_at,
-			       WEEK(sales_flat_order.created_at, 7) AS semana,
-			       LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) AS `year`,
-			       COUNT(sales_flat_order.total_paid) AS pedidos,
-			       SUM(sales_flat_order.total_paid) AS monto,
-			       sales_flat_order.status
-			       FROM magento.sales_flat_order sales_flat_order
-			       WHERE (sales_flat_order.status = 'complete')
-			       GROUP BY WEEK(sales_flat_order.created_at, 7)";
+$query_tecnolite_venta = "SELECT COUNT(sales_flat_order.total_paid) AS Pedidos,
+		       SUM(sales_flat_order.total_paid) AS Venta,
+		       WEEK(sales_flat_order.created_at, 7) AS Semana,
+		       LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) AS `Year`,
+		       Subquery.fraudes
+		       FROM magento.sales_flat_order sales_flat_order
+		       LEFT OUTER JOIN
+		       (SELECT COUNT(DISTINCT sales_flat_order.increment_id) AS fraudes,
+		       WEEK(sales_flat_order.created_at, 7) AS semana,
+		       LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) AS ```year```
+		       FROM magento.sales_flat_order_status_history sales_flat_order_status_history
+		       INNER JOIN magento.sales_flat_order sales_flat_order
+		       ON (sales_flat_order_status_history.parent_id = sales_flat_order.entity_id)
+		       WHERE sales_flat_order_status_history.comment LIKE '%declined%'
+		       GROUP BY WEEK(sales_flat_order.created_at, 7),
+		       LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4)) Subquery
+		       ON     (LEFT(YEARWEEK(sales_flat_order.created_at, 7), 4) = Subquery.```year```)
+		       AND (WEEK(sales_flat_order.created_at, 7) = Subquery.semana)
+		       WHERE sales_flat_order.status IN ('complete', 'processing') 
+		       AND (YEARWEEK(sales_flat_order.created_at, 7) > YEARWEEK(CURDATE(), 7)-5) 
+		       GROUP BY WEEK(sales_flat_order.created_at, 7) ";
 
 ?>

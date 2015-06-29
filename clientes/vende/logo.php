e2a71d6389d57f1ab95a97e2a9cb9fb67d1fc6f1
danielvende <daniel@vende.io>
2015-06-29 16:12:54 -0500

    se crea consulta para pedir logo desde db

--- clientes/vende/logo.php
+++ clientes/vende/logo.php
@@ -1,4 +1,26 @@
+<?php 
+	ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    require_once("../../config/db.php");
+    require_once("../../classes/Login.php");
+    $login = new Login();
+
+        if ($login->isUserLoggedIn() == false) 
+        {
+            echo "<script>
+                location.href='index.php';
+                </script>";
+        }
+    $cliente = $_SESSION['user_email'];
+    
+    $con_logo   = new mysqli("104.236.137.39","admin_fotos","9Fdvi3D4LR","admin_sistemaproductos");
+    $query_logo = "SELECT url_logo FROM sistema_multicliente WHERE cliente = 'VENDE'";
+    $consulta_logo = mysqli_query($con_logo, $query_logo);
+
+?>
+
 
 <div class="col-sm-2">
-		<img src="http://vende.io/wp-content/uploads/2015/03/Logo-rect-130px.png" style="width: 8em;">
+		<img src="<?php echo $consulta_logo; ?>" style="width: 8em;">
 </div>

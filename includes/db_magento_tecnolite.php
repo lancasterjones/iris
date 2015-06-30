7ef709e006455eff9bf641f4a40ef2a97a448e57
danielvende <daniel@vende.io>
2015-06-30 10:12:05 -0500

    conexión a db usuario root

--- includes/db_magento_tecnolite.php
+++ includes/db_magento_tecnolite.php
@@ -1,14 +1,14 @@
 <?php
 
 	$server_tecnolite  = "192.241.212.235";
-	$usuario_tecnolite = "iris";
-	$pass_tecnolite    = "iris";
+	$usuario_tecnolite = "root";
+	$pass_tecnolite    = "u8eJ[R?5dynf9@j";
 	$db_name_tecnolite = "magento";
 
 	$con_tecnolite = new mysqli($server_tecnolite, $usuario_tecnolite, $pass_tecnolite, $db_name_tecnolite)
 						or die("Error " . mysqli_error($con_tecnolite));
 
 	if ($con_tecnolite->connect_error) {
-    	die("Conexión a DB Tecnolite falló: " . $con_tecnolite->connect_error);
-	} else echo "Conexión Tecnolite OK.";
+    	die("Conexion a DB Tecnolite fallo: " . $con_tecnolite->connect_error);
+	} else echo "Conexion Tecnolite OK.";
 ?>

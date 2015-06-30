a376ea07b99b432396a592d423b8e574b1466e17
lancasterjones <lancasterjones@gmail.com>
2015-06-30 11:01:15 -0500

    Cambio a mysql en lugar de mysqli

--- includes/db_magento_tecnolite.php
+++ includes/db_magento_tecnolite.php
@@ -4,11 +4,12 @@
 	$pass_tecnolite    = "iris";
 	$db_name_tecnolite = "magento";
 
-	$con_tecnolite = new mysqli($server_tecnolite, $usuario_tecnolite, $pass_tecnolite, $db_name_tecnolite)
+	$con_tecnolite = new mysql_connect($server_tecnolite, $usuario_tecnolite, $pass_tecnolite, $db_name_tecnolite)
 						or die
-							("Error " . mysqli_error($con_tecnolite));
-
+							("Error " /*. mysql_error($con_tecnolite)*/);
+/*
 	if ($con_tecnolite->connect_error) {
     	die("Conexion a DB Tecnolite fallo: " . $con_tecnolite->connect_error);
 	} else { echo "Conexion Tecnolite OK."; }
-?>
+*/ 
+	?>

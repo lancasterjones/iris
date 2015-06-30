726d3274332ad2b869288a602307ef6f5cc6d792
danielvende <daniel@vende.io>
2015-06-30 10:35:40 -0500

    conexión a db con password iris

--- clientes/tecnolite/logo.php
+++ clientes/tecnolite/logo.php
@@ -1,4 +1,28 @@
+<?php 
+	ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    require_once("../../config/db.php");
+    require_once("../../classes/Login.php");
+    $login = new Login();
+
+        if ($login->isUserLoggedIn() == false) 
+        {
+            echo "<script>
+                location.href='index.php';
+                </script>";
+        }
+    $cliente = $_SESSION['user_email'];
+    
+    $con_logo   = new mysqli("104.236.137.39","admin_fotos","9Fdvi3D4LR","admin_sistemaproductos");
+    $query_logo = "SELECT url_logo FROM sistema_multicliente WHERE cliente = 'VENDE'";
+    $consulta_logo = mysqli_query($con_logo, $query_logo);
+    while($row = mysqli_fetch_array($consulta_logo)){
+        $logo = $row['url_logo'];
+    }
+?>
+
 
 <div class="col-sm-2">
-		<img src="https://tienda.tecnolite.com.mx/skin/frontend/base/default/images/logotecnoliteL.png" style="width: 10em;">
+		<img src="<?php echo $logo; ?>" style="width: 10em;">
 </div>

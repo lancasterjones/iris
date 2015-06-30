1075969bd7e3783720988d2dc296c2b94e1fd190
lancasterjones <lancasterjones@gmail.com>
2015-06-30 11:04:50 -0500

    Correcciones conexión

--- includes/db_magento_tecnolite.php
+++ includes/db_magento_tecnolite.php
@@ -4,11 +4,13 @@
 	$pass_tecnolite    = "iris";
 	$db_name_tecnolite = "magento";
 
-	$con_tecnolite = new mysql_connect($server_tecnolite, $usuario_tecnolite, $pass_tecnolite, $db_name_tecnolite)
+	$con_tecnolite = mysql_connect($server_tecnolite, $usuario_tecnolite, $pass_tecnolite)
 						or die
 							("Error " /*. mysql_error($con_tecnolite)*/);
 	print "Connectado a MySQL<br>";
-	mysql_close($db_name_tecnolite);
+	mysql_close($con_tecnolite);
+
+
 /*
 	if ($con_tecnolite->connect_error) {
     	die("Conexion a DB Tecnolite fallo: " . $con_tecnolite->connect_error);

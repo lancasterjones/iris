fa0b57243187926d4dec4f72777283d4fa8d92a9
danielvende <daniel@vende.io>
2015-07-01 14:11:48 -0500

    crear estrcutura de formulario y select

---
+++ configurar_cliente.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-offset-3 col-md-9">
+			<form class="form-inline">
+				<div class="form-group">
+					<select class="form-control">
+						<option>LOB</option>
+						<option>Tecnolite</option>
+					</select>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>

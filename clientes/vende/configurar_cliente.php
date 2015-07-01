ca7723c74850e67db9a86a8d62b7445e5f5205fe
danielvende <daniel@vende.io>
2015-07-01 14:31:07 -0500

    crear input url

--- clientes/vende/configurar_cliente.php
+++ clientes/vende/configurar_cliente.php
@@ -1,9 +1,18 @@
-<legend>Configurar Vista de Cliente</legend>
-<form class="form-inline">
-	<div class="form-group">
-		<select class="form-control">
-			<option>LOB</option>
-			<option>Tecnolite</option>
-		</select>
-	</div>
-</form>
+<div class="container" style="border-top-style: solid; border-color: #E7E7E6; border-width: 3px;">
+	<legend>Configurar Vista de Cliente</legend>
+	<form class="form-inline">
+		<div class="form-group">
+			<label class="control-label">Seleccionar cliente: </label>
+			<select class="form-control">
+				<option></option>
+				<option>LOB</option>
+				<option>Tecnolite</option>
+			</select>
+		</div>
+
+		<div class="form-group">
+			<label class="control-label">Logo: </label>
+			<input type="text" class="form-control" placeholder="url">
+		</div>
+	</form>
+</div>

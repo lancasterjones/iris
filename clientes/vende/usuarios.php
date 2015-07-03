e3e9620908c3e025f89c8fc677e864c67dbeafdc
danielvende <daniel@vende.io>
2015-07-03 10:55:33 -0500

    insertar todos los usuarios registrados

--- clientes/vende/usuarios.php
+++ clientes/vende/usuarios.php
@@ -1,7 +1,24 @@
+<?php
+	//conexión a base de datos
+	$servidor = "104.236.137.39";
+	$db_name  = "admin_sistemaproductos";
+	$usuario  = "admin_fotos";
+	$pass     = "9Fdvi3D4LR";
+
+	$conect = new mysqli($servidor, $usuario, $pass, $db_name)
+			  or die("Imposible conectar a DB");
+
+	$query = "SELECT users.user_name AS usuario, 
+					 users.user_email AS empresa
+  			FROM admin_sistemaproductos.users users"
+  	$consulta = mysqli_query($conect, $query);
+
+?>
+
 <div class="container-fluid">
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<legend class="panel-title"></legend>
+			<legend class="panel-title">Accesos</legend>
 		</div>
 		<div class="panel-body">
 			<table class="table table-hover">
@@ -10,14 +27,23 @@
 					<th>Compañía</th>
 				</thead>
 				<tbody>
-					<tr>
-						<td>
-							<h5>tecnolite</h5>
-						</td>
-						<td>
-							<h5>Vende</h5>
-						</td>
-					</tr>
+
+					<?php
+						while($row = mysqli_fetch_array($consulta))
+							{	
+					?>
+								<tr>
+									<td>
+										<h5> <?php echo $row['usuario']; ?> </h5>
+									</td>
+									<td>
+										<h5> <?php echo $row['empresa']; ?> </h5>
+									</td>
+								</tr>
+
+					<?php
+							}
+					?>					
 				</tbody>
 			</table>
 		</div>

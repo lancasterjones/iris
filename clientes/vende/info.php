feddcc975b6a859d995dea9473f4752b4be9a09b
danielvende <daniel@vende.io>
2015-07-03 10:58:23 -0500

    separar elementos en columnas distintas

--- clientes/vende/info.php
+++ clientes/vende/info.php
@@ -9,10 +9,12 @@
 					<tr>
 						<td>
 							<i class="fa fa-refresh" style="color: #00B88A; font-size: 20px;"></i>
-							<h4>Última sincronización</h4>
+						</td>
+						<td>							
+							<h5>Última sincronización</h5>
 						</td>
 						<td>
-							<h4>28/06/2015</h4>
+							<h5>28/06/2015</h5>
 						</td>
 					</tr>
 					
